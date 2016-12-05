0cb81982a9534bdca178d3e8d727db9a1b00b9e9
Celine Yuwono <celineyuwono@gmail.com>
2016-12-05 01:49:21 +0000

    Add text

--- build-presentation.sh
+++ build-presentation.sh
@@ -2,7 +2,7 @@
 
 INPUT=$1
 TEMPLATE=template-index.html
-TRANSITION=slide
-THEME=black
+TRANSITION=convex
+THEME=beige
 
 pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" $INPUT.md -o index.html
